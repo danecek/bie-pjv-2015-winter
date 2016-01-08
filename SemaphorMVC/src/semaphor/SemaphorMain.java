6ed1306f3dcf56a396804720334ea150629cdd07
danecek <danecek@fit.cvut.cz>
2016-01-08 17:51:24 +0100

    Merge origin/master
    Conflicts:
    	.gitignore

--- SemaphorMVC/src/semaphor/SemaphorMain.java
+++ SemaphorMVC/src/semaphor/SemaphorMain.java
@@ -26,49 +26,69 @@ import semaphor.view.SemaphorView;
  */
 public class SemaphorMain extends Application {
 
-    SemaphorThread st;
+    private SemaphorThread st;
+    private SemaphorModel sm;
+
+    private class StartButton extends Button {
+
+        StartButton() {
+            super("Start");
+            setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if (st == null) {
+                        st = new SemaphorThread(sm);
+                        st.start();
+                    }
+                }
+            });
+        }
+    }
+
+    private class StopButton extends Button {
+
+        StopButton() {
+            super("Stop");
+            setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if (st != null) {
+                        st.myStop();
+                        st=null;
+                    }
+                }
+            });
+        }
+    }
+
+    private class NextButton extends Button {
+
+        NextButton() {
+            super("Next");
+            setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    sm.goToNextState();
+                }
+            });
+        }
+    }
+
+    private class ButtonBox extends HBox {
+
+        public ButtonBox() {
+            super(5, new NextButton(), new StartButton(), new StopButton());
+            setAlignment(Pos.CENTER);
+            setPadding(new Insets(5));
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) {
-        SemaphorModel sm = new SemaphorModel();
+        sm = new SemaphorModel();
         SemaphorView sd = new SemaphorView(30, sm, Color.RED, Color.YELLOW, Color.GREEN);
-        Button startButton = new Button("Start");
-        startButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (st == null) {
-                    st = new SemaphorThread(sm);
-                    st.start();
-                }
 
-            }
-        });
-        Button stopButton = new Button("Stop");
-        stopButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (st != null) {
-                    st.myStop();
-                }
-                st = null;
-            }
-        });
-
-        Button nextButton = new Button("Next");
-        nextButton.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                sm.goToNextState();
-                sd.fire();
-            }
-        });
-        HBox buttonBox = new HBox(nextButton, startButton, stopButton);
-        buttonBox.setAlignment(Pos.CENTER);
-        buttonBox.setSpacing(5);
-        buttonBox.setPadding(new Insets(5));
-
-        VBox root = new VBox(sd, buttonBox);
+        VBox root = new VBox(sd, new ButtonBox());
         root.setAlignment(Pos.CENTER);
 
         sd.fire();
